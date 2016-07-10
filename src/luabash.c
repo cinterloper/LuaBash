b69ee4a3d4bdd2c1902b57180bc5d7876e68486c
g <grant@iowntheinter.net>
2016-07-10 16:39:16 -0700

    small patch for luajit

--- src/luabash.c
+++ src/luabash.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "lua.h"
-#include "lauxlib.h"
-#include "lualib.h"
+#include "luajit-2.0/lua.h"
+#include "luajit-2.0/lauxlib.h"
+#include "luajit-2.0/lualib.h"
 
 
 #define EXECUTION_SUCCESS 0
